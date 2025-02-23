6f394fcc1530f84e387e68a3e9c3d679927a073d
MASIHULLAH OMAR <masihullahomar@MASIHULLAHs-MacBook-Air.local>
2025-02-23 16:58:38 +0300

    Add new project files and updated config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 BASE_URL = "https://api.themoviedb.org/3"
-API_KEY = "147470ba037df98abe63e570bae097cd"
+API_KEY = "your_api_key_here"
 USERS_FILE = "backend/database/users.json"
 USER_DATA_FOLDER = "backend/database/user_movie_data"
 IMAGES_FOLDER = "backend/database/movie_images"
